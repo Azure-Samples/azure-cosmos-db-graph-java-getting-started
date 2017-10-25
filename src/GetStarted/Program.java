b744c4756d7191ec7bda3d4abc00fb617918b931
Luis Bosquez <luisdbosquez@gmail.com>
2017-10-25 16:25:25 -0700

    Misc fixes
    - Made the gremlin commands a global constant, to follow Java best practices
    - Added print statements for every part of the execution of the program.
    - Added the "press Enter to exit" kind of statement at the end of the demo.
    - Added comments on query execution and connection instantiation.

--- src/GetStarted/Program.java
+++ src/GetStarted/Program.java
@@ -7,12 +7,41 @@ import org.apache.tinkerpop.gremlin.driver.ResultSet;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+
 public class Program
 {
+        /*
+            Example Gremlin queries to perform the following:
+            - add vertices and edges
+            - query with filters, projections, 
+            - traversals, including loops
+            - update annd delete vertices and edges
+        */
+        static final String gremlinQueries[] = new String[] {
+            "g.V().drop()",
+            "g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44)",
+            "g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39)",
+            "g.addV('person').property('id', 'ben').property('firstName', 'Ben').property('lastName', 'Miller')",
+            "g.addV('person').property('id', 'robin').property('firstName', 'Robin').property('lastName', 'Wakefield')",
+            "g.V('thomas').addE('knows').to(g.V('mary'))",
+            "g.V('thomas').addE('knows').to(g.V('ben'))",
+            "g.V('ben').addE('knows').to(g.V('robin'))",
+            "g.V('thomas').property('age', 44)",
+            "g.V().count()",
+            "g.V().hasLabel('person').has('age', gt(40))",
+            "g.V().hasLabel('person').order().by('firstName', decr)",
+            "g.V('thomas').outE('knows').inV().hasLabel('person')",
+            "g.V('thomas').outE('knows').inV().hasLabel('person').outE('knows').inV().hasLabel('person')",
+            "g.V('thomas').repeat(out()).until(has('id', 'robin')).path()",
+            "g.V('thomas').outE('knows').where(inV().has('id', 'mary')).drop()",
+            "g.V('thomas').drop()" };
+
+
     public static void main( String[] args ) throws ExecutionException, InterruptedException {
 
 
@@ -30,50 +59,39 @@ public class Program
         Client client;
 
         try {
+            // Attempt to create the connection objects
             cluster = Cluster.build(new File("src/remote.yaml")).create();
+            client = cluster.connect();
         } catch (FileNotFoundException e) {
+            // Handle file errors.
+            System.out.println("Couldn't find the configuration file.");
             e.printStackTrace();
             return;
         }
 
-        client = cluster.connect();
+        // After connection is successful, run all the queries against the server.
+        for (String query : gremlinQueries) {
+            System.out.println("\nSubmitting this Gremlin query: " + query);
 
-        /*
-            Example Gremlin queries to perform the following:
-            - add vertices and edges
-            - query with filters, projections, 
-            - traversals, including loops
-            - update annd delete vertices and edges
-        */
-        String gremlinQueries[] = new String[] {
-            "g.V().drop()",
-            "g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44)",
-            "g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39)",
-            "g.addV('person').property('id', 'ben').property('firstName', 'Ben').property('lastName', 'Miller')",
-            "g.addV('person').property('id', 'robin').property('firstName', 'Robin').property('lastName', 'Wakefield')",
-            "g.V('thomas').addE('knows').to(g.V('mary'))",
-            "g.V('thomas').addE('knows').to(g.V('ben'))",
-            "g.V('ben').addE('knows').to(g.V('robin'))",
-            "g.V('thomas').property('age', 44)",
-            "g.V().count()",
-            "g.V().hasLabel('person').has('age', gt(40))",
-            "g.V().hasLabel('person').order().by('firstName', decr)",
-            "g.V('thomas').outE('knows').inV().hasLabel('person')",
-            "g.V('thomas').outE('knows').inV().hasLabel('person').outE('knows').inV().hasLabel('person')",
-            "g.V('thomas').repeat(out()).until(has('id', 'robin')).path()",
-            "g.V('thomas').outE('knows').where(inV().has('id', 'mary')).drop()",
-            "g.E().count()",
-            "g.V('thomas').drop()" };
-
-        for (String gremlin : gremlinQueries) {
-            ResultSet results = client.submit(gremlin);
+            // Submitting remote query to the server.
+            ResultSet results = results = client.submit(query);
 
             CompletableFuture<List<Result>> completableFutureResults = results.all();
             List<Result> resultList = completableFutureResults.get();
 
             for (Result result : resultList) {
+                System.out.println("\nQuery result:");
                 System.out.println(result.toString());
             }
         }
+
+        System.out.println("Demo complete!\n Press Enter key to continue...");
+        try{
+            System.in.read();
+        } catch (IOException e){
+            e.printStackTrace();
+            return;
+        }
+        System.exit(0);
     }
 }
